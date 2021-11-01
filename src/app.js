945e0eadc7d3412f1be53b58f0cab7bad02c4d8c
Lewiisa <92801289+Lewiisa@users.noreply.github.com>
2021-11-01 11:46:34 +0100

    JS date and time implemented

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 let apiKey = "97c2f6a3b34509ac62090edc5d18d949";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -8,11 +30,13 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windspeedElement = document.querySelector("#windSpeed");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = response.data.main.temp;
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windspeedElement.innerHTML = response.data.wind.speed;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
